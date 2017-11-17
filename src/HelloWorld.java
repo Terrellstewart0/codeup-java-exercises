63269de7baf85f29bab8ecdf77ab899d98d24883
terrellstewart <terrellstewart0@gmail.com>
2017-11-17 09:07:42 -0600

    Added stuff

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,40 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World");
+        int myFavoriteNumber;
+        myFavoriteNumber = 25;
+        myFavoriteNumber = 43;
+
+        System.out.println("my favorite number is " + myFavoriteNumber);
+
+
+        String myString = "String";
+        System.out.println("whats good fam " + myString);
+
+//        error cant assign chars to string variable
+//        myString = 'c';
+
+//        myString = 3.141;
+
+        float myNumber;
+//        System.out.println(myNumber);
+
+        myNumber = 123L;
+        System.out.println(myNumber);
+
+
+        myNumber = 123;
+        System.out.println(myNumber);
+
+//        myNumber = 3.141;
+//        System.out.println(myNumber);
+
+
+        int x = 5;
+        System.out.println(x++);
+        System.out.println(x);
+
+
+
+
     }
 }
