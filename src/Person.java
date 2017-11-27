cf1af6a2ecaa1c127c68fbb58cf6f36ead5b3fd5
terrellstewart <terrellstewart0@gmail.com>
2017-11-27 16:44:27 -0600

    Added stuff

--- src/Person.java
+++ src/Person.java
@@ -1,3 +1,22 @@
 public class Person {
 
+   private static String name;
+
+    public static void main(String[] args) {
+
+
+
+
+
+
+        // returns the person's name
+        public String getName();
+// changes the name property to the passed value
+        public void setName();
+// prints a message to the console using the person's name
+        public void sayHello();
+
+
+
+    }
 }
